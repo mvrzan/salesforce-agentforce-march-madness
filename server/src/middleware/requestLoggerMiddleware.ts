52a8b8b2f9810ad02dd01d1c348c2be552ae5f6a
mvrzan <mvrzan@icloud.com>
2026-03-16 10:11:23 -0400

    Add request logger middleware for improved request logging

---
+++ server/src/middleware/requestLoggerMiddleware.ts
@@ -0,0 +1,13 @@
+import { type Request, type Response, type NextFunction } from "express";
+import { logger } from "../utils/loggingUtil.ts";
+
+export const requestLoggerMiddleware = (req: Request, res: Response, next: NextFunction): void => {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    logger.info("requestLoggerMiddleware.ts", `${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`);
+  });
+
+  next();
+};
