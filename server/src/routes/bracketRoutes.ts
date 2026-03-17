479221c5f36c2ec991d0dc38c35546b2e6c25ebc
mvrzan <mvrzan@icloud.com>
2026-03-17 16:08:27 -0400

    remove bracketRoutes file and associated saveBracket route

--- server/src/routes/bracketRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { saveBracket } from "../controllers/bracketController.ts";
-
-const bracketRoutes = Router();
-
-bracketRoutes.post("/api/v1/bracket/save", saveBracket);
-
-export default bracketRoutes;
