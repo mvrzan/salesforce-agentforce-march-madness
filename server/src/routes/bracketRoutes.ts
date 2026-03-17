9cf76fb72d7f5201b1bbd445da1718ab9156a472
mvrzan <mvrzan@icloud.com>
2026-03-17 13:57:57 -0400

    Remove unused getBracket and scoreUserVsReal endpoints to streamline bracketRoutes

--- server/src/routes/bracketRoutes.ts
+++ server/src/routes/bracketRoutes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { saveBracket, getBracket, scoreUserVsReal } from "../controllers/bracketController.ts";
+import { saveBracket } from "../controllers/bracketController.ts";
 
 const bracketRoutes = Router();
 
 bracketRoutes.post("/api/v1/bracket/save", saveBracket);
-bracketRoutes.get("/api/v1/bracket/:id", getBracket);
-bracketRoutes.get("/api/v1/bracket/:id/score", scoreUserVsReal);
 
 export default bracketRoutes;
