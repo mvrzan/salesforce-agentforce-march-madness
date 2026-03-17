a94e0a7fb3725e1d8a096c0490afc59f5bf816b8
mvrzan <mvrzan@icloud.com>
2026-03-17 13:57:54 -0400

    Remove unused sendStreamingMessage endpoint to streamline agentforce API routes

--- server/src/routes/agentforceApi.ts
+++ server/src/routes/agentforceApi.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import startSession from "../controllers/startSession.ts";
 import deleteSession from "../controllers/deleteSession.ts";
 import { validateSignature } from "../middleware/validateSignature.ts";
-import sendStreamingMessage from "../controllers/sendStreamingMessage.ts";
 import streamBracketRound from "../controllers/streamBracketRound.ts";
 import streamBracketRetry from "../controllers/streamBracketRetry.ts";
 import streamBracketAdapt from "../controllers/streamBracketAdapt.ts";
@@ -11,7 +10,6 @@ const agentforceApiRoutes = Router();
 
 agentforceApiRoutes.post("/api/v1/af/sessions/:sessionId", validateSignature, startSession);
 agentforceApiRoutes.delete("/api/v1/delete-session", validateSignature, deleteSession);
-agentforceApiRoutes.post("/api/v1/send-streaming-message", validateSignature, sendStreamingMessage);
 agentforceApiRoutes.post("/api/v1/af/bracket/round", validateSignature, streamBracketRound);
 agentforceApiRoutes.post("/api/v1/af/bracket/retry", validateSignature, streamBracketRetry);
 agentforceApiRoutes.post("/api/v1/af/bracket/adapt", validateSignature, streamBracketAdapt);
